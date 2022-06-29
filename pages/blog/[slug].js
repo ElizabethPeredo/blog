ef493acf3eaaa1fa65250ab6886b3c5074a81e3b
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2022-06-28 22:45:11 -0500

    Post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,59 +1,61 @@
+import React from 'react'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import { marked } from 'marked'
 import Link from 'next/link'
-
+import PostContainer from '../../src/containers/PostContainer/PostContainer'
+import PostContainerInfo from '../../src/containers/PostContainer/PostContainerInfo'
 export default function PostPage({
-  frontmatter: { title, date, cover_image },
-  slug,
-  content,
-}) {
-  return (
-    <>
-      <Link href='/'>
-        <a className='btn btn-back'>Go Back</a>
-      </Link>
-      <div className='card card-page'>
-        <h1 className='post-title'>{title}</h1>
-        <div className='post-date'>Posted on {date}</div>
-        <img src={cover_image} alt='' />
-        <div className='post-body'>
-        <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
-        </div>
-      </div>
-    </>
-  )
+    frontmatter: { title, date, cover_image },
+    slug,
+    content, }) {
+    return (
+        <>
+            <PostContainerInfo>
+                <img src={cover_image} alt='' />
+            </PostContainerInfo>
+            <PostContainer>
+                <h1 id="title">{title}</h1>
+                <color1>Fecha de publicación: {date}</color1>
+                <hr />
+                <div dangerouslySetInnerHTML=
+                    {{ __html: marked.parse(content), }}
+                >
+                </div>
+            </PostContainer>
+        </>
+    )
 }
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync(path.join('posts'))
+    const files = fs.readdirSync(path.join('posts'))
 
-  const paths = files.map((filename) => ({
-    params: {
-      slug: filename.replace('.md', ''),
-    },
-  }))
+    const paths = files.map((filename) => ({
+        params: {
+            slug: filename.replace('.md', ''),
+        },
+    }))
 
-  return {
-    paths,
-    fallback: false,
-  }
+    return {
+        paths,
+        fallback: false,
+    }
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const markdownWithMeta = fs.readFileSync(
-    path.join('posts', slug + '.md'),
-    'utf-8'
-  )
+    const markdownWithMeta = fs.readFileSync(
+        path.join('posts', slug + '.md'),
+        'utf-8'
+    )
 
-  const { data: frontmatter, content } = matter(markdownWithMeta)
+    const { data: frontmatter, content } = matter(markdownWithMeta)
 
-  return {
-    props: {
-      frontmatter,
-      slug,
-      content,
-    },
-  }
+    return {
+        props: {
+            frontmatter,
+            slug,
+            content,
+        },
+    }
 }
--- src/components/Header/Banner/Banner.js
+++ src/components/Header/Banner/Banner.js
@@ -9,7 +9,7 @@ export default function Banner() {
                     <h1>My name is <span>Elizabeth Peredo</span></h1>
                     <p>lorem ipsum dolor sit amet, consectet lorem ipsum dolor lorem ipsum dolor sit am lorem ipsum dolor sit am lorem ipsum dolor sit amet, consectet lorem ipsum dolor lorem ipsum dolor sit am
                     </p>
-                    <Link href='/page/contacto'>
+                    <Link href='/'>
                         <a className="btn">Contacto</a>
                     </Link>
                 </div>
