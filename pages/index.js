6fac6d19b4b0138854d2849c4344875c395201b9
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2022-06-30 12:04:37 -0500

    Metadata

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,21 @@ import Image from 'next/image'
 import Banner from '../src/components/Header/Banner/Banner'
 import About from '../src/components/Layout/About'
 import Contact from '../src/components/Layout/Contact'
+import Head from 'next/head'
 
 export default function Home() {
     return (
         <>
-        <Banner/>
-        <About/>
-        <Contact/>
+            <Head>
+                <title>Elizabeth Peredi</title>
+                <meta name="keywords" content="elizabeth peredo mescco, elizabeth peredo, blog" />
+                <meta name="author" content="Elizabet Peredo" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Head>
+            <Banner />
+            <About />
+            <Contact />
         </>
-        
+
     )
 }
